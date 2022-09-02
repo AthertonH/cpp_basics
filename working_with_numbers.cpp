2d4c43a1f3426f6b9274572e99d8252b5361dd15
AthertonH <athertonharrison@gmail.com>
2022-09-01 19:27:14 -0700

    init

---
+++ working_with_numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// fmin()
+// fmax()
+// sqrt()
+// pwr()
+// floor()
+// ceil()
+// round()
+
+
+int main()
+{
+    
+
+    cout << fmax(3,10);
+
+    return 0;
+}
