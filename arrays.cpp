76122777adb02a27eed349bd2e291f2f64de1cb1
AthertonH <athertonharrison@gmail.com>
2022-09-10 20:33:28 -0700

    init

--- arrays.cpp
+++ arrays.cpp
@@ -5,10 +5,15 @@ using namespace std;
 
 int main()
 {
+    int luckyNums[20];
+
+    luckyNums[0] = 200;
+    luckyNums[10] = 100;
+
+    cout << luckyNums[0];
 
 
 
 
-
-    return 0
+    return 0;
 }
---
+++ functions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void sayHi()
+{
+    cout << "Hello User";
+}
+
+int main()
+{
+
+    return 0;
+}
