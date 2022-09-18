28aef872fc53a23603246c129e23e48447556557
AthertonH <athertonharrison@gmail.com>
2022-09-17 22:44:38 -0700

    init

--- 2d_arrays_and_nested_loops.cpp
+++ 2d_arrays_and_nested_loops.cpp
@@ -10,7 +10,15 @@ int main()
                             {3, 4},
                             {5, 6}
                         };
-    cout << numberGrid[2][0];
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 2; j++){
+            cout << numberGrid[i][j];
+
+        cout << endl
+        }
+    }
+
 
     return 0;
 }
